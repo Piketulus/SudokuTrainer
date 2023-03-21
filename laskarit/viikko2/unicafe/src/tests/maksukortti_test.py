9733c04a42a169d6d51ec9234447df30d961acc3
Joel <joel.pietilainen@helsinki.fi>
2023-03-21 22:34:42 +0200

    test vs code commit

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,17 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+# tee testit tänne
+#-Kortin saldo alussa oikein
+#-Rahan lataaminen kasvattaa saldoa oikein
+#-Rahan ottaminen toimii:
+#-Saldo vähenee oikein, jos rahaa on tarpeeksi
+#-Saldo ei muutu, jos rahaa ei ole tarpeeksi
+#-Metodi palauttaa True, jos rahat riittivät ja muuten False
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
