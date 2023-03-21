4f03e01b358c55e68b7d4bd7bce9047b83daeb5c
Joel <joel.pietilainen@helsinki.fi>
2023-03-21 22:39:52 +0200

    vscode test 2

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,13 +7,6 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
-# tee testit tänne
-#-Kortin saldo alussa oikein
-#-Rahan lataaminen kasvattaa saldoa oikein
-#-Rahan ottaminen toimii:
-#-Saldo vähenee oikein, jos rahaa on tarpeeksi
-#-Saldo ei muutu, jos rahaa ei ole tarpeeksi
-#-Metodi palauttaa True, jos rahat riittivät ja muuten False
 
     def test_kortin_saldo_alussa_oikein(self):
         self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
@@ -21,3 +14,19 @@ class TestMaksukortti(unittest.TestCase):
     def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
         self.maksukortti.lataa_rahaa(1000)
         self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+
+    def test_rahan_ottaminen_toimii(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+    
+    def test_saldo_ei_muutu(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahat_riittaa_true(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_rahat_riittaa_false(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.ota_rahaa(600), False)
