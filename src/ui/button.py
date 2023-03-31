293292e1cc2bbd9f2006ff3c2863cad9f468d347
Joel <joel.pietilainen@helsinki.fi>
2023-03-31 13:20:51 +0300

    ui

---
+++ src/ui/button.py
@@ -0,0 +1,31 @@
+import pygame
+
+class Button:
+
+    def __init__(self, x, y, width, height, text, normalcolor, hovercolor, text_color, font, action=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.color = normalcolor
+        self.normalcolor = normalcolor
+        self.hovercolor = hovercolor
+        self.text_color = text_color
+        self.font = font
+        self.action = action
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        text = self.font.render(self.text, 1, self.text_color)
+        screen.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+    
+    def process(self):
+        mousepos = pygame.mouse.get_pos()
+        self.color = self.normalcolor
+        if self.x < mousepos[0] < self.x + self.width and self.y < mousepos[1] < self.y + self.height:
+            if pygame.mouse.get_pressed()[0]:
+                if self.action != None:
+                    self.action()
+            else:
+                self.color = self.hovercolor
--- src/ui/start.py
+++ src/ui/start.py
@@ -1,12 +1,14 @@
 import pygame
-from .objects import Button
+from .button import Button
 
 class Start:
 
     def start(self):
         pygame.init()
         pygame.display.set_caption("Sudoku")
-        startButton = Button(100, 100, 200, 50, "Start", (255, 255, 255), (0, 0, 0), pygame.font.SysFont("comicsans", 40))
+        objects = []
+        startButton = Button(100, 100, 200, 50, "Start", (255, 255, 255), (200, 200, 200), (0, 0, 0), pygame.font.SysFont("comicsans", 30), self.quitScreen)
+        objects.append(startButton)
         screen = pygame.display.set_mode((400, 600))
         screen.fill((255,192,203))
         pygame.display.flip()
@@ -15,12 +17,11 @@ class Start:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    pos = pygame.mouse.get_pos()
-                    startButton.click(pos)
-
-            startButton.draw(screen)
-            pygame.display.update()
+                    return
+            for obj in objects:
+                obj.process()
+                obj.draw(screen)
+            pygame.display.flip()
                 
     def showScreen(self):
         self.start()
