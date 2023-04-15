d725116e04cd9557e6c7319f81cc96a3368c7806
Joel <joel.pietilainen@helsinki.fi>
2023-04-15 22:28:45 +0300

    pylint

--- tasks.py
+++ tasks.py
@@ -12,6 +12,16 @@ def test(ctx):
 
 
 @task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+
+
+@task
+def format(ctx):  # pylint: disable=redefined-builtin
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
+
+
+@task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
 
